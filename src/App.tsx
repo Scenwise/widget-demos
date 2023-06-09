184cf01cee089a8d566c2e5cf2b840aff9578e2c
Elena Dumitrescu <e.i.dumitrescu-1@student.tudelft.nl>
2023-06-09 14:57:42 +0200

    Format files with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Box } from '@mui/material';
-import WidgetWrapper from './components/WidgetWrapper';
-import CongestionGraph from './components/dashboard/CongestionGraph'
-import VehiclePassingGraph from './components/dashboard/VehiclePassingGraph'
-import ParkingWidget from './components/parkingWidget/ParkingWidget';
-import RiskMap from './components/riskmap/RiskMap'
-import IntersectionDashboard from './components/dashboard/IntersectionDashboard';
+import { Box } from "@mui/material";
+import WidgetWrapper from "./components/WidgetWrapper";
+import CongestionGraph from "./components/dashboard/CongestionGraph";
+import VehiclePassingGraph from "./components/dashboard/VehiclePassingGraph";
+import ParkingWidget from "./components/parkingWidget/ParkingWidget";
+import RiskMap from "./components/riskmap/RiskMap";
+import IntersectionDashboard from "./components/dashboard/IntersectionDashboard";
 
 function App() {
   return (
     <Box
       sx={{
-        width: '100vw',
-        height: '100vh',
-        placeItems: 'center',
-        display: 'grid',
+        width: "100vw",
+        height: "100vh",
+        placeItems: "center",
+        display: "grid",
       }}
     >
       <WidgetWrapper>
--- src/components/riskmap/RiskMap.tsx
+++ src/components/riskmap/RiskMap.tsx
@@ -1,16 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { useEffect, useRef, useState} from "react";
-import * as XLSX from 'xlsx';
+import React, { useEffect, useRef, useState } from "react";
+import * as XLSX from "xlsx";
 import { AccidentData } from "../../data/interfaces/AccidentData";
 import { set } from "immer/dist/internal";
 
 const RiskMap = () => {
   // Parse the Excel file to retrieve the accidents
   let accidents = useRef<Array<AccidentData>>([]);
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     // Note: all excel files should stay in the "public" folder for them to be parsed
-    const filePath =  "./accidents-excel/brabant2022.xlsx";
+    const filePath = "./accidents-excel/brabant2022.xlsx";
     const fetchData = async () => {
       const response = await fetch(filePath);
       const data = await response.arrayBuffer();
@@ -18,9 +18,11 @@ const RiskMap = () => {
       const worksheetName = workbook.SheetNames[0];
       const worksheet = workbook.Sheets[worksheetName];
       // Construct the AccidentData array
-      const JSONdata =  XLSX.utils.sheet_to_json(worksheet) as Array<AccidentData>;
+      const JSONdata = XLSX.utils.sheet_to_json(
+        worksheet
+      ) as Array<AccidentData>;
       accidents.current = JSONdata;
-      setLoading(false)
+      setLoading(false);
     };
     fetchData();
   }, []);
