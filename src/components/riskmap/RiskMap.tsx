d91af251d38a4f4484716417168592257ed33b11
Elena Dumitrescu <e.i.dumitrescu-1@student.tudelft.nl>
2023-06-28 14:29:39 +0200

    Add filtering

--- src/components/riskmap/RiskMap.tsx
+++ src/components/riskmap/RiskMap.tsx
@@ -8,25 +8,35 @@ import {
   Paper,
   Stack,
   Typography,
-} from '@mui/material';
+  Select,
+  MenuItem,
+  SelectChangeEvent,
+} from "@mui/material";
+import ClearIcon from "@mui/icons-material/Clear";
 import React, { useEffect, useRef, useState } from "react";
 import * as XLSX from "xlsx";
 import { AccidentData } from "../../data/interfaces/AccidentData";
 import { set } from "immer/dist/internal";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store";
-import { LngLatLike } from 'mapbox-gl';
+import { LngLatLike } from "mapbox-gl";
 import featureCollectionConverter from "./featureCollectionConverter";
-import MapBoxContainer from '../MapBoxContainer';
-import AccidentLocationListItem from './AccidentLocationListItem';
+import MapBoxContainer from "../MapBoxContainer";
+import AccidentLocationListItem from "./AccidentLocationListItem";
 
 const RiskMap = () => {
   // Parse the Excel file to retrieve the accidents
   const [accidentData, setAccidentData] = useState<Array<AccidentData>>([]);
   const [loading, setLoading] = useState(true);
   const [map, setMap] = useState<mapboxgl.Map | null>(null);
-  const [geoJSONDataPoint, setDataJSONDataPoint] = useState<GeoJSON.FeatureCollection<GeoJSON.Geometry>>();
-  const [geoJSONDataSegment, setDataJSONDataSegment] = useState<GeoJSON.FeatureCollection<GeoJSON.Geometry>>();
+  const [geoJSONDataPoint, setDataJSONDataPoint] =
+    useState<GeoJSON.FeatureCollection<GeoJSON.Geometry>>();
+  const [geoJSONDataSegment, setDataJSONDataSegment] =
+    useState<GeoJSON.FeatureCollection<GeoJSON.Geometry>>();
+  const [selectedProcesses, setSelectedProcesses] = useState<string[]>([]);
+  const [filteredAccidentData, setFilteredAccidentData] = useState<
+    Array<AccidentData>
+  >([]);
 
   useEffect(() => {
     // if (!map) return;
@@ -37,56 +47,62 @@ const RiskMap = () => {
       const data = await response.arrayBuffer();
       const workbook = XLSX.read(data, { type: "array", cellDates: true });
       const worksheetName = workbook.SheetNames[0];
-      const worksheet = workbook.Sheets[worksheetName];    
+      const worksheet = workbook.Sheets[worksheetName];
       const JSONdata = XLSX.utils.sheet_to_json(
         worksheet
       ) as Array<AccidentData>;
 
       setAccidentData(JSONdata);
-      
-      const {featureCollectionPoint, featureCollectionSegment} = featureCollectionConverter(JSONdata);
+      setFilteredAccidentData(JSONdata);
 
-      setDataJSONDataPoint(featureCollectionPoint as GeoJSON.FeatureCollection<GeoJSON.Geometry>);
-      setDataJSONDataSegment(featureCollectionSegment as GeoJSON.FeatureCollection<GeoJSON.Geometry>);
+      const { featureCollectionPoint, featureCollectionSegment } =
+        featureCollectionConverter(JSONdata);
+
+      setDataJSONDataPoint(
+        featureCollectionPoint as GeoJSON.FeatureCollection<GeoJSON.Geometry>
+      );
+      setDataJSONDataSegment(
+        featureCollectionSegment as GeoJSON.FeatureCollection<GeoJSON.Geometry>
+      );
       setLoading(false);
     };
     fetchData();
-    
-    map?.addSource('accidentsLocationsSourcePoint', {
-      type: 'geojson',
+
+    map?.addSource("accidentsLocationsSourcePoint", {
+      type: "geojson",
       data: geoJSONDataPoint as GeoJSON.FeatureCollection,
     });
     map?.addLayer({
-      id: 'accidentsLayerPoint',
-      type: 'circle',
-      source: 'accidentsLocationsSourcePoint',
+      id: "accidentsLayerPoint",
+      type: "circle",
+      source: "accidentsLocationsSourcePoint",
       layout: {},
       paint: {
-        'circle-color': 'red',
-        'circle-radius': 6,
-        'circle-stroke-color': '#FFF',
-        'circle-stroke-width': 2,
+        "circle-color": "red",
+        "circle-radius": 6,
+        "circle-stroke-color": "#FFF",
+        "circle-stroke-width": 2,
       },
     });
 
-    map?.addSource('accidentsLocationsSourceSegment', {
-      type: 'geojson',
+    map?.addSource("accidentsLocationsSourceSegment", {
+      type: "geojson",
       data: geoJSONDataSegment as GeoJSON.FeatureCollection,
     });
     map?.addLayer({
-      id: 'accidentsLayerSegment',
-      type: 'line',
-      source: 'accidentsLocationsSourceSegment',
+      id: "accidentsLayerSegment",
+      type: "line",
+      source: "accidentsLocationsSourceSegment",
       layout: {},
       paint: {
         "line-color": "orange",
-        "line-width": 3
-      }
+        "line-width": 3,
+      },
     });
   }, [map]);
 
   const { flyToLocation } = useSelector(
-    (state: RootState) => state.accidentsWidget,
+    (state: RootState) => state.accidentsWidget
   );
 
   useEffect(() => {
@@ -96,50 +112,123 @@ const RiskMap = () => {
     map.flyTo({ center: flyToLocation, zoom: 14 });
   }, [map, flyToLocation]);
 
-  // Construct the riskmap based on the accidents (TODO)
+  const handleProcessSelection = (event: SelectChangeEvent<string[]>) => {
+    const selectedValues = Array.isArray(event.target.value)
+      ? event.target.value
+      : [event.target.value];
+
+    // Check if the "Clear" option is selected
+    if (selectedValues.includes("Clear")) {
+      setSelectedProcesses([]);
+      setFilteredAccidentData(accidentData);
+      return;
+    }
+
+    setSelectedProcesses(selectedValues);
+
+    // Filter the accident data based on the selected processes
+    const filteredData =
+      selectedValues.length === 0
+        ? accidentData // Show all accidents if no processes are selected
+        : accidentData.filter((location) =>
+            selectedValues.includes(location.Proces)
+          );
+
+    setFilteredAccidentData(filteredData);
+  };
+
   // To access the accidents, use accidents.current
   return (
     <Stack direction="row" alignItems="stretch" height={400}>
       <Paper
         elevation={0}
-        sx={{ width: '40%', position: 'relative', overflow: 'auto' }}
+        sx={{ width: "40%", position: "relative", overflow: "auto" }}
       >
         <Box
           position="sticky"
-          sx={{ bgcolor: 'background.paper', top: 0, px: 2, pt: 2, zIndex: 1 }}
+          sx={{ bgcolor: "background.paper", top: 0, px: 2, pt: 2, zIndex: 1 }}
         >
           <Typography variant="h6">North Brabant Accidents</Typography>
+          <Box display="flex" alignItems="center" sx={{ width: "100%", mb: 2 }}>
+            <Typography variant="body2" sx={{ paddingRight: 2 }}>
+              Filter by Process
+            </Typography>
+            <Select
+              multiple
+              value={selectedProcesses}
+              onChange={handleProcessSelection}
+              renderValue={(selected) =>
+                (selected as string[]).join(", ").length > 20
+                  ? `${(selected as string[]).join(", ").slice(0, 20)}...`
+                  : (selected as string[]).join(", ")
+              }
+              sx={{
+                minWidth: 200,
+                whiteSpace: "nowrap",
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+                height: 30,
+                padding: 0,
+              }}
+              MenuProps={{
+                sx: { maxHeight: 200 },
+              }}
+            >
+              <MenuItem value="Clear">Clear selections</MenuItem>
+              {Array.from(
+                new Set(accidentData.map((location) => location.Proces))
+              ).map((proces) => (
+                <MenuItem key={proces} value={proces}>
+                  {proces}
+                </MenuItem>
+              ))}
+            </Select>
+          </Box>
         </Box>
+
         <List>
-          <ListSubheader sx={{ top: 48, bgcolor: 'background.paper' }}>
+          <ListSubheader sx={{ top: 40, bgcolor: "background.paper" }}>
             All Accidents
           </ListSubheader>
 
-          {accidentData.map((location) => (
+          {filteredAccidentData.map((location) => (
             <AccidentLocationListItem
               key={location.ID}
               name={location.Weg}
-              location={[location.Longitude_van, location.Latitude_van] as LngLatLike}
+              location={
+                [location.Longitude_van, location.Latitude_van] as LngLatLike
+              }
               zijde={location.Zijde}
-              hmpVan={location['Hmp van']}
-              hmpTot={location['Hmp tot']}
-              ovd={location.ovd?location.ovd.toTimeString(): ""}
+              hmpVan={location["Hmp van"]}
+              hmpTot={location["Hmp tot"]}
+              ovd={location.ovd ? location.ovd.toTimeString() : ""}
               Starttijd={location.Starttijd.toTimeString()}
               Einddatum={location.Einddatum.toDateString()}
-              Eerste_tijd_ter_plaatse={location['Eerste tijd ter plaatse']?location['Eerste tijd ter plaatse'].toTimeString():""}
-              Laatste_eindtijd={location['Laatste eindtijd']?location['Laatste eindtijd'].toTimeString():""}
-              color={location['Hmp tot']?"orange":"red"}
+              Eerste_tijd_ter_plaatse={
+                location["Eerste tijd ter plaatse"]
+                  ? location["Eerste tijd ter plaatse"].toTimeString()
+                  : ""
+              }
+              Laatste_eindtijd={
+                location["Laatste eindtijd"]
+                  ? location["Laatste eindtijd"].toTimeString()
+                  : ""
+              }
+              color={location["Hmp tot"] ? "orange" : "red"}
               Proces={location.Proces}
               Melder={location.Melder}
             />
           ))}
         </List>
-
       </Paper>
 
       <Box p={1} flexGrow={1} width="60%">
-        <Box sx={{ borderRadius: 6, overflow: 'hidden' }} height="100%">
-          <MapBoxContainer mapState={[map, setMap]} location={[5.025243, 51.567082] as LngLatLike} zoomLevel={8} />
+        <Box sx={{ borderRadius: 6, overflow: "hidden" }} height="100%">
+          <MapBoxContainer
+            mapState={[map, setMap]}
+            location={[5.025243, 51.567082] as LngLatLike}
+            zoomLevel={8}
+          />
         </Box>
       </Box>
     </Stack>
