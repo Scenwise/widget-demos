1b626fe36a51d4e4bee019657cbe34b55643f643
Radoslav <R.N.Sozonov@student.tudelft.nl>
2023-07-17 18:19:56 +0300

    Make more generic the way to create point and segments feature collections

--- src/components/riskmap/featureCollectionConverter.ts
+++ src/components/riskmap/featureCollectionConverter.ts
@@ -1,40 +1,29 @@
 import { AccidentData } from "../../data/interfaces/AccidentData";
 import { LngLatLike } from 'mapbox-gl';
 
-const convertToFeaturePoint = (data: AccidentData) => {
-    const feature = {
-        "id": data.ID,
-        "type": "Feature",
-        "geometry": {
-            "type": "Point",
-            "coordinates": [data.Longitude_van, data.Latitude_van] as LngLatLike
-        },
-        "properties": {
-            "gid": data.ID,
-            "weg": data.Weg,
-            "hmp_van": data["Hmp van"],
-            "hmp_tot": data["Hmp tot"],
-            "zijde": data.Zijde,
-            "startdatum": data.Starttijd,
-            "einddatum": data.Einddatum,
-            "proces": data.Proces,
-            "beschrijving": data.Beschrijving,
-            "melder": data.Melder,
-            "Eerste tijd ter plaatse": data["Eerste tijd ter plaatse"],
-            "Laatste eindtijd": data["Laatste eindtijd"],
-            "ovd": data.ovd
-        }
-    };
-    return feature;
+const segmentCoordinates = (data: AccidentData) => {
+    const coordinates: Array<number[]> = []
+    data.Points.split(";").forEach(coordinate => {
+        const lnglat = coordinate.split(",")
+        const points: number[] = []
+        lnglat.forEach(x => points.push(Number.parseFloat(x)))
+        coordinates.push(points)
+    })
+    return coordinates
 }
 
-const convertToFeatureSegment = (data: AccidentData) => {
+const pointCoordinates = (data: AccidentData) => {
+    return [data.Longitude_van, data.Latitude_van] as LngLatLike
+}
+
+const convertToFeature = (data: AccidentData, segment: string) => {
+    const coordinates = segment === "LineString" ? segmentCoordinates(data) : pointCoordinates(data)
     const feature = {
         "id": data.ID,
         "type": "Feature",
         "geometry": {
-            "type": "LineString",
-            "coordinates": [[data.Longitude_van, data.Latitude_van] as LngLatLike, [data.Longitude_tot, data.Latitude_tot] as LngLatLike]
+            "type": segment,
+            "coordinates": coordinates
         },
         "properties": {
             "gid": data.ID,
@@ -66,11 +55,11 @@ const featureCollectionConverter = (dataArray: Array<AccidentData>) => {
         features: [] as Array<unknown>
     }
 
-    dataArray.forEach(x => {
-        x["Hmp tot"]?
-            featureCollectionSegment.features.push(convertToFeatureSegment(x))
+    dataArray.forEach((x) => {
+        x["Hmp tot"] ?
+            featureCollectionSegment.features.push(convertToFeature(x, "LineString"))
             :
-            featureCollectionPoint.features.push(convertToFeaturePoint(x))
+            featureCollectionPoint.features.push(convertToFeature(x, "Point"));
     })
     
     return {featureCollectionPoint, featureCollectionSegment};
